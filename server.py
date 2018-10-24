1ef1416d6f86879076921cd2bd2a5a72b3b3b4aa
mattlink <mattlink570@gmail.com>
2018-10-23 19:15:35 -0500

    make databse more OO & add endpoint for finished departments

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World\n'
+
+
+@app.route('/finished', methods=['GET', 'POST'])
+def get_finished():
+    if request.method == 'GET':
+        db = database.Database('courses.db')
+        return str(db.get_finished_departments())
